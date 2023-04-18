b493bdf3abdb60d85adf5a130283fb6f388bdc4c
maarten <van_den_bergmaarten@hotmail.com>
2023-04-18 11:48:19 +0300

    catching the metaData

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 const multer = require("multer");
+const { stringify } = require("querystring");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "Images");
   },
   filename: (req, file, cb) => {
     console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, file.originalname);
+    // cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
@@ -32,6 +35,23 @@ app.post("/multiple", multerUpload.array("images", 10), (req, res) => {
 
 app.post("/jsonfile", multerUpload.single("jsonfile"), (req, res) => {
   res.send("jsonfile Uploaded");
+});
+
+app.post("/stats", multerUpload.single("uploaded_file"), function (req, res) {
+  // console.log(req.file);
+  // console.log("-------------------------------------");
+  console.log(req.body);
+  const metaData = stringify(req.body);
+  console.log(metaData);
+  res.send(
+    `Succes uploading! ${decodeURI(metaData)}, please go to previous page`
+  );
+  //Now we want to use FS to fsWriteFile(req.body, "./metadata.json")
+  let data = metaData;
+  fs.writeFileSync("metadata.json", data);
+  console.log("File written successfully\n");
+  console.log("The written has the following contents:");
+  console.log(fs.readFileSync("metadata.json", "utf8"));
 });
 
 // non-multer:    MUST BE CHANGED STILL!!!
---
+++ indexEXperimental.js
@@ -0,0 +1,40 @@
+const express = require("express");
+app.use(express.static("view"));
+
+const multer = require("multer");
+const upload = multer({ dest: "./public/data/uploads/" });
+app.post("/stats", upload.single("uploaded_file"), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  console.log(req.file, req.body);
+});
+
+app.post("/profile", upload.single("avatar"), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+});
+
+app.post(
+  "/photos/upload",
+  upload.array("photos", 12),
+  function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  }
+);
+
+const cpUpload = upload.fields([
+  { name: "avatar", maxCount: 1 },
+  { name: "gallery", maxCount: 8 },
+]);
+app.post("/cool-profile", cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+});
+app.listen(3000);
+console.log("3000 is the port");
